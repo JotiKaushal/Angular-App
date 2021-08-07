4c59fa4e4bdc65ca2870ae9f64b4eb196cbbda00
Kapil Sharma <kapil.kumar.sharma4756@gmail.com>
2021-08-07 18:43:42 +0530

    commit for next features

---
+++ src/app/recipes/recipe.service.ts
@@ -0,0 +1,34 @@
+import { EventEmitter, Injectable } from "@angular/core";
+import { Ingredient } from "../shared/ingredient.model";
+import { ShoppingListService } from "../shopping-list/shopping-list.service";
+import { Recipe } from "./recipe.model";
+
+@Injectable()
+export class RecipeService{
+constructor(private slService : ShoppingListService)
+{}
+
+   private recipes: Recipe[] =[
+        new Recipe(
+          "Green Salad",
+          "Easy healthy recipe",
+        "https://hips.hearstapps.com/del.h-cdn.co/assets/16/21/1600x1066/gallery-1464124561-shot-1-033.jpg?resize=980:*",
+        [new Ingredient('Meat',1),
+        new Ingredient('French Fries',20)]),
+        new Recipe(
+          "Vegetarian Mapo Tofu",
+          "Easy healthy recipe",
+        "https://media.self.com/photos/60634a7b6fb177c75e545c84/4:3/w_384/Vegetarian-Mapo-Tofu.jpg",
+        [new Ingredient('Meat',1),
+        new Ingredient('Buns',2)]),
+    ];
+
+      recipeSelected = new EventEmitter<Recipe>();
+      getRecipe(){
+          return this.recipes.slice();//will return copy of original array
+      }
+
+      addIngredientToShoppingList(ingredients: Ingredient[]){
+        this.slService.addIngredients(ingredients);
+      }
+}
